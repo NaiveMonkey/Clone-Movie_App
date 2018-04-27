479806fb2abd7fdabc1b09130e0d1a86a02bab4c
SaemiruKim <ksmr1102@gmail.com>
2018-04-27 23:59:29 +0900

    change app.js by using array map

--- src/App.js
+++ src/App.js
@@ -2,28 +2,32 @@ import React, {Component} from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-    "Matrix",
-    "Full Metal Jacket",
-    "Oldboy",
-    "Star Wars"
-]
-
-const movieImages = [
-    "https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
-    "https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL.jpg",
-    "https://www.languagetrainers.com/reviews/foreign-film-reviews/uploads/9214-Oldboy.jpg",
-    "https://starwarsblog.starwars.com/wp-content/uploads/sites/6/2017/10/the-last-jedi-theatrical-blog.jpg"
+const movies = [
+    {
+        title: "Matrix",
+        poster: "https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
+    },
+    {
+        title: "Full Metal Jacket",
+        poster: "https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL.jpg"
+    },
+    {
+        title: "Oldboy",
+        poster: "https://www.languagetrainers.com/reviews/foreign-film-reviews/uploads/9214-Oldboy.jpg"
+    },
+    {
+        title: "Star Wars",
+        poster: "https://starwarsblog.starwars.com/wp-content/uploads/sites/6/2017/10/the-last-jedi-theatrical-blog.jpg"
+    }
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-          <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-          <Movie title={movieTitles[2]} poster={movieImages[2]}/>
-          <Movie title={movieTitles[3]} poster={movieImages[3]}/>
+          {movies.map(movie => {
+              return <Movie title={movie.title} poster={movie.poster}/>
+          })}
       </div>
     );
   }

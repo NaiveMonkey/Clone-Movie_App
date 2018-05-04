0063984a6e753cb5d55738837ba0e88fae7dedd4
SaemiruKim <ksmr1102@gmail.com>
2018-05-04 21:00:21 +0900

    create renderMovie method

--- src/App.js
+++ src/App.js
@@ -4,48 +4,48 @@ import Movie from './Movie';
 
 class App extends Component {
 
-    state = {
-        greeting: "Hello!",
-        movies: [
-            {
-                title: "Matrix",
-                poster: "https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
-            },
-            {
-                title: "Full Metal Jacket",
-                poster: "https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL.jpg"
-            },
-            {
-                title: "Oldboy",
-                poster: "https://www.languagetrainers.com/reviews/foreign-film-reviews/uploads/9214-Oldboy.jpg"
-            },
-            {
-                title: "Star Wars",
-                poster: "https://starwarsblog.starwars.com/wp-content/uploads/sites/6/2017/10/the-last-jedi-theatrical-blog.jpg"
-            }
-        ]
-    }
+    state = {}
 
     componentDidMount() {
         setTimeout(() => {
             this.setState({
                 movies: [
-                    ...this.state.movies,
+                    {
+                        title: "Matrix",
+                        poster: "https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
+                    },
+                    {
+                        title: "Full Metal Jacket",
+                        poster: "https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL.jpg"
+                    },
+                    {
+                        title: "Oldboy",
+                        poster: "https://www.languagetrainers.com/reviews/foreign-film-reviews/uploads/9214-Oldboy.jpg"
+                    },
+                    {
+                        title: "Star Wars",
+                        poster: "https://starwarsblog.starwars.com/wp-content/uploads/sites/6/2017/10/the-last-jedi-theatrical-blog.jpg"
+                    },
                     {
                         title: "Head Hunter",
                         poster: "https://greecetonorway.files.wordpress.com/2014/03/headhunters-2011-movie-poster.jpg?w=665"
                     }
                 ]
             })
-        }, 2000)
+        }, 5000)
+    }
+
+    _renderMovies = () => {
+        const movies = this.state.movies.map((movie, index) => {
+            return <Movie title={movie.title} poster={movie.poster} key={index}/>
+        })
+        return movies
     }
 
     render() {
         return (
             <div className="App">
-                {this.state.movies.map((movie, index) => {
-                    return <Movie title={movie.title} poster={movie.poster} key={index}/>
-                })}
+                {this.state.movies ? this._renderMovies() : 'Loading'}
             </div>
         );
     }

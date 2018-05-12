2c9d89f933b3f33949df34f88174d40111aa380c
SaemiruKim <ksmr1102@gmail.com>
2018-05-12 15:25:23 +0900

    create get movie method from api by using async

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import Movie from './Movie';
 
 class App extends Component {
 
-    state = {}
+    state = {};
 
     componentDidMount() {
-        fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+        this._getMovies();
     }
 
     _renderMovies = () => {
-        const movies = this.state.movies.map((movie, index) => {
-            return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })
+        const movies = this.state.movies.map(movie => {
+            return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
+        });
         return movies
-    }
+    };
+
+     _getMovies = async () => {
+         const movies = await this._callApi();
+         this.setState({
+             movies
+         })
+     };
+
+
+    _callApi = () => {
+        // fetch 를 통헤 url 을 ajax 로 불러올 수 있
+        return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+            .then(response => response.json())
+            .then(json => json.data.movies)
+            .catch(err => console.log(err))
+    };
 
     render() {
         return (
